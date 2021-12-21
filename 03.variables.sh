8abc0a9f528dab08d71409564891fc0bfcd88272
manjusha <gmanjusmart@gmail.com>
2021-12-21 16:55:49 +0000

    variables

---
+++ 03.variables.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# assigning a name to set of data is called as variable 
+# plain variable variable = data
+a=10 # number type data
+b=xyz # character type data
+c=true #boolean type data
+d=abc123 # string type data
+
+# shell will conisder every type of data as string 
+# if data is having special characters then it has to be written in double quotes
+
+e="1*2"
+
+# accessing of variables
+
+echo value of a = $a
+echo value of b = ${b}
