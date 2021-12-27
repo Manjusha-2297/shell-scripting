bf3973257336385934d14c4b9c278b5c55da0b5b
manjusha <gmanjusmart@gmail.com>
2021-12-27 05:23:42 +0000

    comitting mongodb file

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -10,18 +10,22 @@ gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mong
 echo "Installing mongodb"
 yum install -y mongodb-org >/tmp/log # sending the output to the /tmp/log file
 
-
+echo "Configuring mongodb"
 # Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 # Config file: /etc/mongod.conf
 
+echo "starting mongodb"
 systemctl enable mongod
 systemctl start mongod
 
+echo "Downloading mongodb schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 
 cd /tmp
+echo "extracting schema archive"
 unzip -o mongodb.zip  >/tmp/log # -o to over ride the file rather than asking every time
 cd mongodb-main
+echo "Loading schema"
 mongo < catalogue.js >/tmp/log
 mongo < users.js >/tmp/log
