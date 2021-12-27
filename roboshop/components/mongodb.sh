fd1b8b2f49c4fe496d6b7fcbdb231a6c46a3bbed
manjusha <gmanjusmart@gmail.com>
2021-12-27 07:19:35 +0000

    comitting mongodb file

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -10,7 +10,12 @@ Status_Check() {
     fi
     
 }
-echo "setting up mongodb repo"
+
+print ()
+{
+    echo -n -e "$1 " # to get in the same line
+}
+print "setting up mongodb repo"
 
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
@@ -20,31 +25,31 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 Status_Check $?
 
-echo "Installing mongodb"
+print "Installing mongodb"
 yum install -y mongodb-org &>>/tmp/log # sending the output to the /tmp/log file
 Status_Check
 
-echo "Configuring mongodb"
+print "Configuring mongodb"
 # Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 # Config file: /etc/mongod.conf
 Status_Check $?
 
-echo "starting mongodb"
+print "starting mongodb"
 systemctl enable mongod
 systemctl start mongod
 Status_Check $?
 
-echo "Downloading mongodb schema"
+print "Downloading mongodb schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 Status_Check $?
 
 cd /tmp
-echo "extracting schema archive"
+print "extracting schema archive"
 unzip -o mongodb.zip  &>>/tmp/log # -o to over ride the file rather than asking every time
 Status_Check $?
 cd mongodb-main
-echo "Loading schema"
+print "Loading schema"
 mongo < catalogue.js &>>/tmp/log
 mongo < users.js &>>/tmp/log
 Status_Check $?
