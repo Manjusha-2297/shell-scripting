c152d81edaab7c6f206e215372fe5899023a5ebd
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2021-12-28 18:33:33 +0530

    pushing frontend code

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -2,7 +2,7 @@
 
 source components/common.sh
 
-print "Install nginx\t"
+print "Install nginx\t\t"
 yum install nginx -y &>>$LOG
 Status_Check $?
 
@@ -14,10 +14,14 @@ print "Extract frontend archive"
 rm -rf /usr/share/nginx/* && cd /usr/share/nginx && unzip -o /tmp/frontend.zip &>>$LOG && mv frontend-main/* . && mv static html &>>$LOG
 Status_Check $?
 
-print "Update nginx config file"
+print "Copy nginx config file"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
 Status_Check $?
 
-print "restart Nginx\t"
+print "Update nginx config file"
+sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' etc/nginx/default.d/roboshop.conf &>>$LOG
+Status_Check $?
+
+print "restart Nginx\t\t"
 systemctl restart nginx &>>$LOG && systemctl enable nginx &>>$LOG
 Status_Check $?
