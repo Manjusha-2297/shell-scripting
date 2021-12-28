e09625d3701b43c74c4739dee82a3cb6cd65977c
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2021-12-28 12:38:32 +0530

    pushing redis code

---
+++ create-instance.sh
@@ -0,0 +1,6 @@
+
+
+LID="lt-0e4f7d924175204ce" # spot instance launch template id
+LVR=2 # launch template version
+
+aws ec2 run-instances --launch-template LaunchTemplateId=$LID Version=$LVR | jq.Instances[].InstanceId|
