331d18cdff78ca93b81421303eaf0bd06f3ed9ca
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2021-12-28 13:05:28 +0530

    pushing redis code

--- create-instance.sh
+++ create-instance.sh
@@ -2,5 +2,12 @@
 
 LID="lt-0e4f7d924175204ce" # spot instance launch template id
 LVR=2 # launch template version
+INSTANCE_NAME=$1
 
-aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVR | jq .Instances[].InstanceId
+if[ -z "${INSTANCE_NAME}" ]; then
+  echo "Input is missing"
+  exit 1
+fi
+
+IP=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVR --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
+# same name to the spot instance and normal instance also
