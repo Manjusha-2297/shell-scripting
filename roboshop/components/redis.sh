2632084f99aaf39ea062881439babe0477529dbd
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2021-12-28 19:56:50 +0530

    pushing frontend code

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -14,12 +14,12 @@ print "install redis\t\t\t"
 yum install redis -y &>>$LOG
 Status_Check $?
 
-print "Configure redis Listner address\t"
+Print "Configure Redis Listen Address\t\t"
 if [ -f /etc/redis.conf ]; then
-sed -i -e 's/127.0.0.1/0.0.0.0/'  /etc/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
 fi
 if [ -f /etc/redis/redis.conf ]; then
-sed -i -e 's/127.0.0.1/0.0.0.0/'  /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
 fi
 Status_Check $?
 
