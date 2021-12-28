ecd8f22d653f645ca7bb329d4696f52e178c4580
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2021-12-28 19:41:04 +0530

    pushing frontend code

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -19,7 +19,7 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
 Status_Check $?
 
 print "Update nginx config file"
-sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' /etc/nginx/default.d/roboshop.conf  &>>$LOG
+sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/' /etc/nginx/default.d/roboshop.conf  &>>$LOG
 Status_Check $?
 
 print "restart Nginx\t\t"
