e4e6f8bffe478a87ce0bd99fb7816da56c6049c3
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2021-12-29 19:46:15 +0530

    pushing rabbitmq code

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -3,7 +3,12 @@
 source components/common.sh
 
 print "install erlang"
+yum list installed| grep erlang &>>$LOG
+if [ $? -eq 0 ]; then
+  echo "package already installed" &>>$LOG
+else
 yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y  &>>$LOG
+fi
 Status_Check $?
 
 print "Setup YUM repositories for RabbitMQ"
