cdc786dc03c0433402e92f71f2c0c089d66ec8d4
Manjusha-gudiwada <manjushagudiwada@gmail.com>
2021-12-29 14:59:42 +0530

    pushing mysql code

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -11,25 +11,20 @@ gpgcheck=0' > /etc/yum.repos.d/mysql.repo
 Status_Check $?
 
 
-print "Install MySQL"
+print "Install MySQL\t"
 yum remove mariadb-libs -y &>>$LOG && yum install mysql-community-server -y &>>$LOG
 Status_Check $?
 
-print "Start MySQL"
+print "Start MySQL\t"
 systemctl enable mysqld &>>$LOG && systemctl start mysqld &>>$LOG
 Status_Check $?
 
+DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}') # to get the last column as it is password
+
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" >/tmp/reset.sql
+mysql -u root -p"${DEFAULT_PASSWORD}" </tmp/reset.sql
+
 exit
-
-Now a default root password will be generated and given in the log file.
-# grep temp /var/log/mysqld.log
-
-Next, We need to change the default root password in order to start using the database service.
-# mysql_secure_installation
-
-You can check the new password working or not using the following command.
-
-# mysql -u root -p
 
 Run the following SQL commands to remove the password policy.
 > uninstall plugin validate_password;
